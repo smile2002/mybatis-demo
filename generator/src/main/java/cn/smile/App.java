d876f56849ac12a2eec12cdd07469956d2cb4d1f
smile2002 <smile2002@foxmail.com>
2018-07-19 15:36:23 +0800

    edit

--- generator/src/main/java/cn/smile/App.java
+++ generator/src/main/java/cn/smile/App.java
@@ -1,13 +1,41 @@
 package cn.smile;
 
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws Exception
     {
         System.out.println( "Hello World!" );
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+
+        InputStream is = App.class.getResourceAsStream("/code-generator-config.xml");
+
+        if (is == null) {
+            System.out.println("is open failed!");
+        }
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(is);
+        is.close();
+
+        DefaultShellCallback cb = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, cb, warnings);
+        myBatisGenerator.generate(null);
+
+        for (String warning : warnings) {
+            System.out.println(warning);
+        }
     }
 }
